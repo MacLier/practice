8fef2260e6d8228ab694d5b53ead596d4ba206ab
MacLier <75802094+MacLier@users.noreply.github.com>
2022-12-19 13:12:14 +0100

    fix: szinkeveres finalize

--- feladat01.js
+++ feladat01.js
@@ -1,8 +1,25 @@
-function valoszinuseg(szinek) {
-    let count = 0;
-    if (szinek.length <= 2) {
-        return 0
+function szinkeveres(szinek) {
+    let osszeg = 0;
+    if (szinek.length < 2) {
+        return 0;
     }
-    return count;
+    for (let i = 2; i < szinek.length; i++) {
+        osszeg += ismetlesNelkuliKombinacio(szinek.length, i);
+    }
+    return osszeg + 1;
 }
-export default valoszinuseg
+
+function faktorialis(n) {
+    let szorzat = 1;
+
+    for (let i = n; i > 0; i--) {
+        szorzat = szorzat * i;
+    }
+    return szorzat;
+}
+
+function ismetlesNelkuliKombinacio(n, k) {
+    return faktorialis(n) / (faktorialis(k) * faktorialis(n - k))
+}
+
+export default szinkeveres;
